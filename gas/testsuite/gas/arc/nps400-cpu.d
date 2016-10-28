7bf8d188a144274a36b0b6941caaab1de4f4ec81
Graham Markall <graham.markall@embecosm.com>
2016-10-28 18:26:40 +0100

    [ARC] Fix ".cpu NPS400"
    An entry for NPS400 was not present in the arch_info_struct, which
    prevents "NPS400" from being considered a valid argument to the
    .cpu directive.
    
    This commit adds an entry for NPS400 and a suitable test.
    
    bfd/ChangeLog:
    
           * cpu-arc.c: Add NPS400 to arch_info_struct.
    
    gas/ChangeLog:
    
           * testsuite/gas/arc/nps400-cpu.d: Added.
           * testsuite/gas/arc/nps400-cpu.s: Added.

---
+++ gas/testsuite/gas/arc/nps400-cpu.d
@@ -0,0 +1,9 @@
+#as:
+#objdump: -dr
+
+.*: +file format .*arc.*
+
+Disassembly of section .text:
+
+[0-9a-f]+ <.*>:
+   0:	3c2e 150a           	rflt	r10,r12,r20
---
+++ gas/testsuite/gas/arc/nps400-cpu.s
@@ -0,0 +1,5 @@
+        .cpu NPS400
+
+        .text
+        rflt           r10,r12,r20
+
